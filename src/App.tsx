86636150097555fb6ae1cc1b81ebd877850ff3b3
Tomas Cuevas <memo_mister398@hotmail.com>
2022-11-04 12:32:44 -0300

    remove useCalcColumns hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
 import { useEffect, useState } from "react";
 import useSWRInmutable from "swr/immutable";
 
+//* hooks *//
+import { useCalcColumns } from "./hooks/useCalcColumns";
+import { useFetchPhotos } from "./hooks/useFetchPhotos";
+
+//* components *//
+import { FeedCard } from "./components/FeedCart";
+import { FeedColumn } from "./components/FeedColumns";
+
 //* interface *//
 import { IPhotos } from "./intefaces/photos";
-import { useCalcColumns } from "./hooks/useCalcColumns";
-import { FeedColumn } from "./components/FeedColumns";
-import { useFetchPhotos } from "./hooks/useFetchPhotos";
 
 export const App = () => {
   const { photos, getNextPage } = useFetchPhotos();
-  const { columns } = useCalcColumns({
-    columnsProps: [
-      { columnsNumber: 1, min_width: 0 },
-      { columnsNumber: 2, min_width: 640 },
-      { columnsNumber: 3, min_width: 1024 },
-    ],
-    elements: photos,
-  });
+  // const { columns } = useCalcColumns({
+  //   columnsProps: [
+  //     { columnsNumber: 1, min_width: 0 },
+  //     { columnsNumber: 2, min_width: 640 },
+  //     { columnsNumber: 3, min_width: 1024 },
+  //   ],
+  //   elements: photos,
+  // });
 
   return (
     <section className="mx-auto mb-10 grid w-full max-w-[900px] grid-cols-1 gap-3 px-[5%] py-6 sm:grid-cols-2 lg:max-w-[1500px] lg:grid-cols-3">
-      {columns.map((column, index) => (
-        <FeedColumn photos={column} key={index} getNextPage={getNextPage} />
+      {photos.map((photo, index) => (
+        <FeedCard {...photo} key={photo.id} />
       ))}
     </section>
   );
